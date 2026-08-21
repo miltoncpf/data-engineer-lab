8acc7fd22e168e2ab8aaf3b373c20eacb8a61438
Milton Prado <miltoncpf@gmail.com>
2026-08-21 18:03:51 -0300

    Evolução do projeto, atualizando arquivo ETL2

--- etl2.py
+++ etl2.py
@@ -1,6 +1,26 @@
+import psycopg2
 import pandas as pd
 
 df = pd.read_csv("clientes.csv")
 df["cidade"] = df["cidade"].fillna("Não informado")
 
-print(df)
+conn = psycopg2.connect(
+    host="postgres",
+    port=5432,
+    dbname="etl",
+    user="postgres",
+    password="postgres"
+)
+
+print("Conectado com sucesso!!!")
+
+cursor = conn.cursor()
+
+for indice, linha in df.iterrows():
+
+    cursor.execute("""
+        INSERT INTO clientes (nome, cidade)
+        VALUES (%s, %s)
+    """, (linha["nome"], linha["cidade"]))
+
+conn.commit()
