2501d47e154f0b6aac760b043f7e48f40c293bbf
Milton Prado <miltoncpf@gmail.com>
2026-09-14 17:58:58 -0300

    Mudando um pouco a abordagem: Iniciando ELT (Não confundir com ETL)

---
+++ _elt_copy.py
@@ -0,0 +1,73 @@
+import psycopg2
+from psycopg2.extras import execute_values
+import pandas as pd
+from io import StringIO
+import time
+
+
+df = pd.read_csv("clientes2.csv")
+df.columns = df.columns.str.strip().str.lower()
+
+conn = psycopg2.connect(
+    host="postgres",
+    port=5432,
+    dbname="etl",
+    user="postgres",
+    password="postgres"
+)
+
+print("Conectado com sucesso!!!")
+cursor = conn.cursor()
+
+cursor.execute(
+	"""CREATE TABLE IF NOT EXISTS raw_clientes(
+	cliente_id INTEGER,
+	nome VARCHAR(200),
+	cidade VARCHAR(100)
+	)"""
+)
+
+cursor.execute("TRUNCATE TABLE raw_clientes")
+
+#inicio do cronômetro
+inicio = time.perf_counter()
+
+buffer = StringIO()
+
+#Utilizando o Copy: convertendo o  df para um fluxo CSV em memória
+df[["cliente_id", "nome", "cidade"]].to_csv(
+	buffer,
+	index=False,
+	header=False
+)
+
+buffer.seek(0)
+
+#Enviando para o Postgres
+cursor.copy_expert(
+    """
+	COPY raw_clientes (cliente_id, nome, cidade)
+	FROM STDIN
+	WITH(
+		FORMAT CSV
+	)
+    """, buffer)
+
+#Upsert set-based:
+cursor.execute(
+	"""
+	INSERT INTO clientes (cliente_id, nome, cidade)
+	SELECT cliente_id, nome, cidade
+	FROM raw_clientes
+
+	ON CONFLICT (cliente_id)
+	DO UPDATE SET
+		nome = EXCLUDED.NOME,
+		cidade = EXCLUDED.cidade
+	""")
+
+conn.commit()
+
+#final do cronometro
+fim = time.perf_counter()
+print(f"Tempo: {fim - inicio:.4f} segundos")
